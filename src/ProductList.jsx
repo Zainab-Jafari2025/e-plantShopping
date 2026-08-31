6e537c31013a941252520c39b0a7c984be2d855e
Zainab Jafari <zainab.jafari2025@gmail.com>
2026-08-31 06:19:16 -0400

    Complete Redux cart state management

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,9 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+import { useDispatch } from 'react-redux';
+import { addItem } from './CartSlice';
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
+    const dispatch = useDispatch();
+const [addedToCart, setAddedToCart] = useState({});
 
     const plantsArray = [
         {
@@ -252,6 +256,14 @@ function ProductList({ onHomeClick }) {
         e.preventDefault();
         setShowCart(false);
     };
+    const handleAddToCart = (product) => {
+        dispatch(addItem(product));
+      
+        setAddedToCart((prevState) => ({
+          ...prevState,
+          [product.name]: true,
+        }));
+      };
     return (
         <div>
             <div className="navbar" style={styleObj}>
@@ -273,10 +285,7 @@ function ProductList({ onHomeClick }) {
                 </div>
             </div>
             {!showCart ? (
-                <div className="product-grid">
-
-
-                </div>
+                
             ) : (
                 <CartItem onContinueShopping={handleContinueShopping} />
             )}
